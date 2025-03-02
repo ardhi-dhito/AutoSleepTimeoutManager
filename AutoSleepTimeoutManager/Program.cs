2389e0393ff401ea8a766c3d4d315a92ffe005de
Ardhianto Raharjo <95130519+ardhi-dhito@users.noreply.github.com>
2025-03-02 11:31:38 +0700

    Integrate Sleep Timeout Configuration in Main Program

--- AutoSleepTimeoutManager/Program.cs
+++ AutoSleepTimeoutManager/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using AutoSleepTimeoutManager.Services;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Masukkan nilai sleep timeout (dalam menit):");
+        if (int.TryParse(Console.ReadLine(), out int timeout) && timeout >= 0)
+        {
+            SleepTimeoutConfigurationService.SetSleepTimeout(timeout);
+            Console.WriteLine($"Sleep timeout berhasil diubah menjadi {timeout} menit.");
+        }
+        else
+        {
+            Console.WriteLine("Input tidak valid. Masukkan angka yang benar.");
+        }
+    }
+}
